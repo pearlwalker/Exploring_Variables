517c8ba1e4308a440ea9ccf188b9c857662b7256
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-11-08 18:27:33 -0500

    heroName variables + Acknowledge hero
    Declared/initialized heroPrefix, heroSuffix, heroName
    
    Greeted heroName

--- Program.cs
+++ Program.cs
@@ -4,9 +4,17 @@ namespace exploring_variables
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Exploring Variables");
+
+            //Variables
+            string heroPrefix = "Pearl"; //I'm doing my warriorsona Pearlstar, leader of...someClan
+            string heroSuffix = "star";
+            string heroName = heroPrefix + heroSuffix;
+
+            //Begin Code Instructions
+            Console.WriteLine("Behold, the leader of PigeonClan - "+ heroName + "!");
         }
     }
 }
