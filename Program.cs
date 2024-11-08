c4dd7ec1546e0e44aa5abca1fca5bf9b803229d3
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-11-08 18:18:33 -0500

    Create Visual Studio project
    Created Visual Studio project, added program files to GitHub repo
    
    Created Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exploring_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exploring Variables");
+        }
+    }
+}
