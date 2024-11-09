8119f8fbb321835e37428192e45e51ae71ee1936
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-11-08 19:00:49 -0500

    Declared Variables
    Declared Variables for program, initialized some
    
    Outlined variables in multiline comment
    
    Changed "-Title" vars to "-Rank" vars to be more accurate to Warrior Cats

--- Program.cs
+++ Program.cs
@@ -6,15 +6,48 @@ namespace exploring_variables
     {
         static void Main()
         {
-            Console.WriteLine("Exploring Variables");
+            Console.WriteLine("Exploring Variables\n");
 
-            //Variables
+            /*
+             * | VARIABLES | 
+             * 
+             *  |Names| 
+             *      - Hero: heroPrefix, heroSuffix, heroRank, heroName
+             *      - Boss: bossRank, bossName
+             *      - Minions: minionRank, minion1Name, minion2Name
+             *  |Stats|
+             *      - Hero: heroHealth, heroStrength
+             *      - Boss: bossHealth, bossStrength
+             *      - Minions: minion1Health, minion2Health, 
+             *                 minion1Strength, minion2Strength
+             */
+
+            // |Names|
             string heroPrefix = "Pearl"; //I'm doing my warriorsona Pearlstar, leader of...someClan
-            string heroSuffix = "star";
-            string heroName = heroPrefix + heroSuffix;
+            string heroSuffix = "star"; //init to -star, bc hero is a leader, but who knows what'll happen
+            string heroRank = "Leader of PigeonClan";
+            string heroName;
+            string bossRank;
+            string bossName;
+            string minionRank;
+            string minion1Name;
+            string minion2Name;
+
+            // |Stats|
+            int heroHealth;
+            int heroStrength;
+            int bossHealth;
+            int bossStrength;
+            int minion1Health;
+            int minion1Strength;
+            int minion2Health;
+            int minion2Strength;
+
 
             //Begin Code Instructions
-            Console.WriteLine("Behold, the leader of PigeonClan - "+ heroName + "!");
+            heroName = heroPrefix + heroSuffix; //no space, it's warrior cats
+            Console.WriteLine("Behold "+ heroName + ", " + heroRank + "!");
+
         }
     }
 }
