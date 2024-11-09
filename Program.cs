4e68d1057d3a68051fa0802980c074f1cd73b5d2
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-11-08 19:15:23 -0500

    Initialized Variables
    Initialized variables for both names and stats.

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ namespace exploring_variables
              *  |Names| 
              *      - Hero: heroPrefix, heroSuffix, heroRank, heroName
              *      - Boss: bossRank, bossName
-             *      - Minions: minionRank, minion1Name, minion2Name
+             *      - Minions: minion1Rank, minion1Name, 
+             *                 minion2Rank, minion2Name
              *  |Stats|
              *      - Hero: heroHealth, heroStrength
              *      - Boss: bossHealth, bossStrength
@@ -26,22 +27,23 @@ namespace exploring_variables
             string heroPrefix = "Pearl"; //I'm doing my warriorsona Pearlstar, leader of...someClan
             string heroSuffix = "star"; //init to -star, bc hero is a leader, but who knows what'll happen
             string heroRank = "Leader of PigeonClan";
-            string heroName;
-            string bossRank;
-            string bossName;
-            string minionRank;
-            string minion1Name;
-            string minion2Name;
+            string heroName = ""; //init to empty string
+            string bossRank = "Leader of BatClan";
+            string bossName = "Fruitstar";
+            string minion1Rank = "BatClan Warrior";
+            string minion1Name = "Whitenose";
+            string minion2Rank = "BatClan Warrior";
+            string minion2Name = "Caveflower";
 
             // |Stats|
-            int heroHealth;
-            int heroStrength;
-            int bossHealth;
-            int bossStrength;
-            int minion1Health;
-            int minion1Strength;
-            int minion2Health;
-            int minion2Strength;
+            int heroHealth = 24;
+            int heroStrength = 36; //health mid, strength high
+            int bossHealth = 48;
+            int bossStrength = 20; //health very high, strength mid
+            int minion1Health = 16;
+            int minion1Strength = 12; //mid stats
+            int minion2Health = 20;
+            int minion2Strength = 8; //health higher mid, stength low
 
 
             //Begin Code Instructions
