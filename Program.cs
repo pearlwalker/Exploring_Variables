8978a5e9a9f5e665cb566d6636259b526160c3d5
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-11-08 19:36:58 -0500

    Introduce characters
    Introduce character names and ranks using Console messages

--- Program.cs
+++ Program.cs
@@ -14,8 +14,7 @@ namespace exploring_variables
              *  |Names| 
              *      - Hero: heroPrefix, heroSuffix, heroRank, heroName
              *      - Boss: bossRank, bossName
-             *      - Minions: minion1Rank, minion1Name, 
-             *                 minion2Rank, minion2Name
+             *      - Minions: minionRank, minion1Name, minion2Name
              *  |Stats|
              *      - Hero: heroHealth, heroStrength
              *      - Boss: bossHealth, bossStrength
@@ -27,18 +26,17 @@ namespace exploring_variables
             string heroPrefix = "Pearl"; //I'm doing my warriorsona Pearlstar, leader of...someClan
             string heroSuffix = "star"; //init to -star, bc hero is a leader, but who knows what'll happen
             string heroRank = "Leader of PigeonClan";
-            string heroName = ""; //init to empty string
+            string heroName = "EMPTY";
             string bossRank = "Leader of BatClan";
-            string bossName = "Fruitstar";
-            string minion1Rank = "BatClan Warrior";
-            string minion1Name = "Whitenose";
-            string minion2Rank = "BatClan Warrior";
-            string minion2Name = "Caveflower";
+            string bossName = "EMPTY";
+            string minionRank = "BatClan Warrior";
+            string minion1Name = "EMPTY";
+            string minion2Name = "EMPTY";
 
             // |Stats|
             int heroHealth = 24;
             int heroStrength = 36; //health mid, strength high
-            int bossHealth = 48;
+            int bossHealth = 44;
             int bossStrength = 20; //health very high, strength mid
             int minion1Health = 16;
             int minion1Strength = 12; //mid stats
@@ -47,9 +45,19 @@ namespace exploring_variables
 
 
             //Begin Code Instructions
-            heroName = heroPrefix + heroSuffix; //no space, it's warrior cats
-            Console.WriteLine("Behold "+ heroName + ", " + heroRank + "!");
+            
+            //Names
+            heroName = heroPrefix + heroSuffix;
+            bossName = "Fruitstar";
+            minion1Name = "Whitenose";
+            minion2Name = "Caveflower";
+
+            //Introduce Characters
+            Console.Write("\"Oh make way for " + heroName + ", the apprentice named " + heroRank + "!\"\n");
+            Console.WriteLine("mocks " + bossName + ", the " + bossRank + ".");
+            Console.WriteLine(bossName + " is accompanied by " + minionRank + "s: " + 
+                minion1Name + " and " + minion2Name + ".");
 
         }
-    }
+    } 
 }
